c2f57ffb07d67e3efd7664ccf5572c627a65c4d5
Killian Saint cricq <Killian Saint cricq>
2016-01-28 13:52:55 +0100

    Book details fragment implementation + on item click first step

--- app/src/main/java/org/emn/ksaintcricq/components/BookRecyclerAdapter.java
+++ app/src/main/java/org/emn/ksaintcricq/components/BookRecyclerAdapter.java
@@ -3,7 +3,9 @@ package org.emn.ksaintcricq.components;
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import org.emn.ksaintcricq.R;
 import org.emn.ksaintcricq.model.Book;
@@ -16,10 +18,21 @@ public class BookRecyclerAdapter extends RecyclerView.Adapter<BookRecyclerAdapte
     private List<Book> books;
     private final Context context;
 
-    public static class ViewHolder extends RecyclerView.ViewHolder {
+    public static class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        private Context context;
+
         public ViewHolder(BookItemView itemView) {
             super(itemView);
+            this.context = itemView.getContext();
+            itemView.setOnClickListener(this);
         }
+
+        @Override
+        public void onClick(View v) {
+            Toast.makeText(context, "Click!", Toast.LENGTH_SHORT).show();
+        }
+
     }
 
     public BookRecyclerAdapter(Context context) {
